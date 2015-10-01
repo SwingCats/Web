9c46354be54a865bd8e076adc54c90903b500e40
Bryan Gin <bryangin23@gmail.com>
2015-09-30 18:12:34 -0700

    Calendar Complete

---
+++ Calendar.php
@@ -0,0 +1,65 @@
+
+<html>
+<?php include("Shared/Header.html"); ?>
+	<link rel="stylesheet" type="text/css" href="Styles/Calendar.css" />
+<?php include("Shared/HeaderBodyBridge.html"); ?>
+	<div id="MainBody">
+		<h2>Calendar</h2>
+		<div class="InfoDetails">
+			<div class="dayDiv Monday">
+				<a class="dayTitle">Monday</a>
+				<div class="events">
+					<div class="eventItem"><span class="time">7:00 - 8:00</span> Lindy Under the Stars lesson</div>
+					<div class="eventItem"><span class="time">8:00 - 10:00</span> Lindy Under the Stars</div>
+					<div class="eventItem"><span class="time">8:00 - 10:30</span> WestCoastSwing : Dance Loft</div>
+				</div>
+			</div>
+			<div class="dayDiv Tuesday">
+				<a class="dayTitle">Tuesday</a>
+				<div class="events">
+					<div class="eventItem"><span class="time">6:00 - 7:00</span> SwingCats WestCoastSwing intermediate lesson</div>
+					<div class="eventItem"><span class="time">7:00 - 8:00</span> SwingCats WestCoastSwing beginner lesson</div>
+				</div>
+			</div>
+			<div class="dayDiv Wednesday">
+				<a class="dayTitle">Wednesday</a>
+				<div class="events">
+				</div>
+			</div>
+			<div class="dayDiv Thursday">
+				<a class="dayTitle">Thursday</a>
+				<div class="events">
+					<div class="eventItem"><span class="time">6:00 - 7:00</span> SwingCats Lindy intermediate lesson</div>
+					<div class="eventItem"><span class="time">7:00 - 8:00</span> SwingCats Lindy beginner lesson</div>
+					<div class="eventItem"><span class="time">7:00 - 8:30</span> WestCoastSwing : Shall we Dance lesson</div>
+					<div class="eventItem"><span class="time">8:30 - 10:30</span> WestCoastSwing : Shall we Dance</div>
+				</div>
+			</div>
+			<div class="dayDiv Friday">
+				<a class="dayTitle">Friday</a>
+				<div class="events">
+					<div class="eventItem"><span class="time">7:00 - 8:00</span> Lindy : Warehouse Stomp lesson</div>
+					<div class="eventItem"><span class="time">7:00 - 8:00</span> Lindy : Warehouse Stomp lesson</div>
+					<div class="eventItem"><span class="time">7:00 - 8:00</span> Lindy : Tucson Stomp Lesson <span class="important">(Second Friday only)</span></div>
+					<div class="eventItem"><span class="time">8:00 - 10:00</span> Lindy : Tucson Stomp <span class="important">(Second Friday only)</span></div>
+				</div>
+			</div>
+			<div class="dayDiv Saturday">
+				<a class="dayTitle">Saturday</a>
+				<div class="events">
+					<div class="eventItem"><span class="time">7:00 - 8:00</span> WestCoastSwing : Racquet Club Lesson <span class="important">(Second Saturday only)</span></div>
+					<div class="eventItem"><span class="time">8:00 - 10:30</span> WestCoastSwing : Racquet Club Lesson <span class="important">(Second Saturday only)</span></div>
+					<div class="eventItem"><span class="time">7:00 - 8:00</span> Lindy : Armory Park Lesson <span class="important">(Fourth Saturday only)</span></div>
+					<div class="eventItem"><span class="time">8:00 - 11:00</span> Lindy : Armory Park Lesson <span class="important">(Fourth Saturday only)</span></div>
+			</div>
+			</div>
+			<div class="dayDiv Sunday">
+				<a class="dayTitle">Sunday</a>
+				<div class="events">		
+					<div class="eventItem"><span class="time">4:30 - 6:30</span> WestCoastSwing : Studio West<span class="important">(Fourth Saturday only)</span></div>			
+				</div>
+			</div>
+		</div>
+	</div>
+<?php include("Shared/Footer.html"); ?>
+</html>
